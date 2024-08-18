2bab8c95a9a3dabded5706f5666a098e2d7faa23
aquacat1220 <aquacat1220@gmail.com>
2024-08-18 18:30:41 +0000

    feat: Add sticky header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Initial commit</div>
+  <div class="sticky top-0 flex h-10 items-center justify-center bg-gray-700">
+    <div class="flex basis-[1000px] justify-between">
+      <div class="p-2 text-xl font-bold text-white">paper-tree</div>
+      <div class="p-2 text-xl font-bold text-white">@main</div>
+    </div>
+  </div>
 </template>
