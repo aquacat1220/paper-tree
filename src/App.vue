5319ed2e70cf05b4c28426299151782eb4188f6c
aquacat1220 <aquacat1220@gmail.com>
2024-08-19 14:27:41 +0000

    refactor: Move some styles from global to App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex h-screen w-screen flex-col items-center">
+  <div class="flex h-screen w-screen flex-col items-center overflow-hidden bg-neutral-900">
     <div class="flex h-10 w-full items-center justify-center bg-neutral-800">
       <div class="flex basis-[50rem] justify-between">
         <div class="p-2 text-xl font-bold text-white">paper-tree</div>
