718210ac918ef1a57ba7c01829cc64512a4433c9
aquacat1220 <aquacat1220@gmail.com>
2024-08-19 14:29:10 +0000

    feat: Update branch name in header

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <div class="flex h-10 w-full items-center justify-center bg-neutral-800">
       <div class="flex basis-[50rem] justify-between">
         <div class="p-2 text-xl font-bold text-white">paper-tree</div>
-        <div class="p-2 text-xl font-bold text-white">@main</div>
+        <div class="p-2 text-xl font-bold text-white">@play/lc/simple</div>
       </div>
     </div>
   </div>
