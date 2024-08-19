04407977d0cec8385416d992484e18990644daa7
aquacat1220 <aquacat1220@gmail.com>
2024-08-19 10:50:26 +0000

    feat: Modify layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template class="">
-  <div class="flex h-screen w-screen flex-col">
-    <div class="flex h-10 items-center justify-center bg-gray-700">
+  <div class="flex h-screen w-screen flex-col items-center">
+    <div class="flex h-10 w-full items-center justify-center bg-gray-800">
       <div class="flex basis-[50rem] justify-between">
         <div class="p-2 text-xl font-bold text-white">paper-tree</div>
         <div class="p-2 text-xl font-bold text-white">@main</div>
