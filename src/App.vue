cc8165d29143df16c9125070363a405af1978749
aquacat1220 <aquacat1220@gmail.com>
2024-08-19 09:58:28 +0000

    feat: Have App.vue fill the screen

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts"></script>
 
-<template>
-  <div class="sticky top-0 flex h-10 items-center justify-center bg-gray-700">
-    <div class="flex basis-[1000px] justify-between">
-      <div class="p-2 text-xl font-bold text-white">paper-tree</div>
-      <div class="p-2 text-xl font-bold text-white">@main</div>
+<template class="">
+  <div class="flex h-screen w-screen flex-col">
+    <div class="flex h-10 items-center justify-center bg-gray-700">
+      <div class="flex basis-[50rem] justify-between">
+        <div class="p-2 text-xl font-bold text-white">paper-tree</div>
+        <div class="p-2 text-xl font-bold text-white">@main</div>
+      </div>
     </div>
   </div>
 </template>
