571a68c7f23f3b693e8b89f3eccc1b3ca1371764
aquacat1220 <aquacat1220@gmail.com>
2024-08-20 15:19:20 +0000

    feat: Add placeholder LlmHistory.vue

---
+++ src/llm/LlmHistory.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  history: {
+    role: string
+    content: string
+  }[]
+}>()
+
+const history = props.history
+</script>
+
+<template>
+  <div class="flex min-h-16 min-w-[30rem] flex-col overflow-auto bg-red-300">
+    <template v-for="message in history" :key="message.content">
+      <div class="bg-blue-300">{{ message }}</div>
+    </template>
+  </div>
+</template>
--- src/llm/LlmSimple.vue
+++ src/llm/LlmSimple.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import LlmInput from './LlmInput.vue'
+import LlmHistory from './LlmHistory.vue'
 
-const onSent = (input: string) => {
-  placeholder.value += ` New input: ${input}`
+let onSent = (input: string) => {
+  history.value.push({ role: 'Human', content: input })
 }
 
-const placeholder = ref('Placeholder for LLM responses.')
+const history = ref([
+  { role: 'Ai', content: 'Hi I am an AI assistant.' },
+  { role: 'Human', content: 'Tell me about your favorite joke.' }
+])
 </script>
 
 <template>
-  <div class="flex flex-col items-center">
-    <div class="w-full max-w-[75rem] grow bg-red-200">{{ placeholder }}</div>
+  <div class="flex min-h-[15rem] flex-col items-center">
+    <LlmHistory class="w-full max-w-[75rem] grow" :history="history" />
     <LlmInput class="mb-14 max-h-[20rem] w-full max-w-[50rem] flex-none" @sent="onSent" />
   </div>
 </template>
