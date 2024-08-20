3ee4152882774389088784fe8aaea8f31ca42b41
aquacat1220 <aquacat1220@gmail.com>
2024-08-20 16:19:13 +0000

    build: Configure project to use vue-cookies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import VueCookies from 'vue-cookies'
 
-createApp(App).mount('#app')
+createApp(App).use(VueCookies).mount('#app')
