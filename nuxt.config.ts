42efbf4994343c28a120b02253316f32d2f6c83a
aquacat1220 <aquacat1220@gmail.com>
2024-08-21 21:18:31 +0000

    fix: Follow nuxt guideline for github pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: process.env.DEPLOY === "github-pages" ? "paper-tree" : "/",
-  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
   modules: ["@nuxt/eslint", "@nuxt/ui"],
