8ac5cbe0bcbba82a9ed47a08c78d0d72b987623a
aquacat1220 <aquacat1220@gmail.com>
2024-08-21 21:41:56 +0000

    feat: Add /index.vue

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="flex min-h-[15rem] min-w-[30rem] flex-col items-center justify-center"
+  >
+    <NuxtLink class="text-3xl text-black dark:text-white" to="/play/simple">
+      /play/simple
+    </NuxtLink>
+  </div>
+</template>
